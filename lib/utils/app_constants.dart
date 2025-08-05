883e9026bb3b2694d70abbf2bb841939481fcae1
salman-rafii <salmanrafi125@gmail.com>
2025-08-05 15:55:39 +0500

    added the app constants & dimensions

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String baseUrl = 'BASE_URL';
+  static const String onboarding = 'ONBOARDING';
+  static const String isSignedIn = 'IS_SIGNED_IN';
+  static const String token = 'TOKEN';
+}
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,22 @@
+class Dimensions {
+  static const double zero = 0;
+  static const double fontSizeExtraSmall = 10.0;
+  static const double fontSizeSmall = 12.0;
+  static const double fontSizeDefault = 14.0;
+  static const double fontSizeLarge = 16.0;
+  static const double fontSizeExtraLarge = 18.0;
+  static const double fontSizeOverLarge = 24.0;
+  static const double fontSizeSuperLarge = 32.0;
+  static const double paddingSizeExtraSmall = 8.0;
+  static const double paddingSizeSmall = 12.0;
+  static const double paddingSizeDefault = 16.0;
+  static const double paddingSizeLarge = 20.0;
+  static const double paddingSizeExtraLarge = 24.0;
+  static const double paddingSizeSuperLarge = 32.0;
+  static const double paddingSizeBottomSpace = 40.0;
+  static const double radiusSmall = 8.0;
+  static const double radiusDefault = 12.0;
+  static const double radiusLarge = 15.0;
+  static const double radiusExtraLarge = 20.0;
+  static const int messageInputLength = 250;
+}
